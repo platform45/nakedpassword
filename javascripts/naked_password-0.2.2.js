1741d71c421a325ac30ec57fad2483a343358bd0
Michael Ficarra <github@michael.ficarra.me>
2011-03-12 11:29:32 -0500

    major refactoring

--- javascripts/naked_password-0.2.2.js
+++ javascripts/naked_password-0.2.2.js
@@ -6,88 +6,65 @@
  * Dual licensed under the MIT or GPL Version 2 licenses.
  */
 
-jQuery.fn.nakedPassword = function (options) {
-return this.each (function () {
- 
-var defaults = {path : "images/", width: 30, height: 28};
-var settings = $.extend(defaults, options);
- 
- 
-var prev_password_level = 0;
- 
-var trigger = function(e) {
-var forward = false;
-password_level = getPasswordStrength($(this).val());
-if(prev_password_level <= password_level){
-forward = true;
-}
- 
-toggleImg($(this).attr("id"), password_level);
- 
-};//end trigger
- 
-function toggleImg(field, level){
-	for(i = 0; i <= 5; i++){
-		if(i == level){
-			$("#" + field + "pic" + i).fadeIn();
-		}else{
-			$("#" + field + "pic" + i).fadeOut();
-		}
-	}
-}
+jQuery.fn.nakedPassword = function(options){
+	return this.each(function(){
+		var defaults =
+				{ path : "images/"
+				, width: 30
+				, height: 28
+				},
+			settings = $.extend(defaults, options);
 
-function getPasswordStrength(password){
-var score = 0;
- 
-//if password bigger than 5 give 1 point
-if (password.length > 5) { score++; }
- 
-//if password has both lower and uppercase characters give 1 point
-if ( ( password.match(/[a-z]/) ) && ( password.match(/[A-Z]/) ) ) { score++; }
- 
-//if password has at least one number AND at least 1 other character give 1 point
-if ( ( password.match(/\d+/) ) && ( password.match(/\D+/) ) ) { score++; }
- 
-//if password has a combination of other characters and special caracthers give 1 point
-if ( ( password.match(/[!,@,#,$,%,^,&,*,?,_,~,-,(,)]+/) ) && ( password.match(/\w+/) ) ) { score++; }
- 
-//if password bigger than 12 give another 1 point (thanks reddit)
-if (password.length > 12) { score++; }
- 
-return score;
-}
- 
- 
- 
-position = $(this).position();
-input_height = $(this).outerHeight();
-input_width = $(this).outerWidth();
- 
-pic_width = (((input_height-6)/settings.height)*settings.width); //30 x 28
-pic_height = input_height - 6;
- 
-properties = {
-            position: 'absolute',
-						display: 'none',
-            opacity: 1.0,
-            left: (position.left + input_width - (pic_width+3)) + "px",
-            top: (position.top + 3) + "px",
-            margin: 0 + "px"
-          };
- 
-if($.browser.safari){
-            properties.marginTop = 3 + "px";
-          }
-          else{
-            properties.marginTop = 1 + "px";
-          }
- 
- 
-for(i = 0; i <= 5; i++){
-	$(this).after("<div style='display:none;' id='" + $(this).attr("id") + "pic" + i + "'><img src='" + settings.path + i + ".png' width='" + pic_width + "' height='" + pic_height + "px' /></div>");
-	$("#" + $(this).attr("id") + "pic" + i).css(properties);
-}
- 
-$(this).bind('keyup', trigger).bind('blur', trigger);
-});
+		function trigger(){
+			var password_level = getPasswordStrength($(this).val());
+			toggleImg($(this).attr("id"), password_level);
+		};
+
+		function toggleImg(field, level){
+			for(i = 0; i <= 5; i++){
+				if(i == level){
+					$("#" + field + "pic" + i).fadeIn();
+				} else {
+					$("#" + field + "pic" + i).fadeOut();
+				}
+			}
+		}
+
+		function getPasswordStrength(password){
+			return 0
+			// if password bigger than 5 give 1 point
+			+ +( password.length > 5 )
+			// if password has both lower and uppercase characters give 1 point
+			+ +( /[a-z]/.test(password) && /[A-Z]/.test(password) )
+			// if password has at least one number and at least 1 other character give 1 point
+			+ +( /\d/.test(password) && /\D/.test(password) )
+			// if password has a combination of other characters and special characters give 1 point
+			+ +( /[!,@,#,$,%,^,&,*,?,_,~,-,(,)]/.test(password) && /\w/.test(password) )
+			// if password bigger than 12 give another 1 point (thanks reddit)
+			+ +( password.length > 12 )
+		}
+
+		var position = $(this).position(),
+			input_height = $(this).outerHeight(),
+			input_width = $(this).outerWidth(),
+			// maybe this magic number `6` should be documented?
+			pic_width = ((input_height - 6) / settings.height) * settings.width,
+			pic_height = input_height - 6,
+			properties =
+				{ position: 'absolute'
+				, display: 'none'
+				, opacity: 1.0
+				, left: (position.left + input_width - (pic_width+3)) + "px"
+				, top: (position.top + 3) + "px"
+				, margin: 0 + "px"
+				, marginTop: ($.browser.safari ? 3 : 1) + "px"
+				};
+
+		for(var i = 0; i <= 5; i++){
+			$(this).after("<div style='display:none;' id='" + $(this).attr("id") + "pic" + i + "'><img src='" + settings.path + i + ".png' width='" + pic_width + "' height='" + pic_height + "px' /></div>");
+			$("#" + $(this).attr("id") + "pic" + i).css(properties);
+		}
+
+		$(this).bind('keyup', trigger).bind('blur', trigger);
+	});
 };
